af0422a9c82333c5fae72c2cfde5a4f548c1fb82
eldanaomushova <eldana956@gmail.com>
2024-12-28 21:18:35 +0600

    Fix error deploy

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,9 +1,9 @@
-import { Events } from "@modules/MainModule/events/Events";
-import { Places } from "@modules/MainModule/places/Places";
 import { About } from "@modules/mainModule/aboutInfo/About";
 import { AboutUs } from "@modules/mainModule/aboutUs/components/AboutUs";
+import { Events } from "@modules/mainModule/event/Events";
 import { MainBlock } from "@modules/mainModule/mainblock/MainBlock";
 import { OurTeam } from "@modules/mainModule/ourTeam/components/OurTeam";
+import { Places } from "@modules/mainModule/places/Places";
 import { TryOurApp } from "@modules/mainModule/tryOurApp/TryOurApp";
 import { Container } from "@ui/container/Container";
 
