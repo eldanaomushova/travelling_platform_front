bc7732723f89a398ef986d99db485a2f8a151647
eldanaomushova <eldana956@gmail.com>
2024-12-28 21:31:37 +0600

    Fix error deploy 4

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { AppRouter } from "./app/routes/AppRouter";
 import { RouterProvider } from "react-router-dom";
 import { auth } from "@utils/config/Config";
 import { onAuthStateChanged } from "firebase/auth";
-import "./app/styles/index.module.scss";
+import "./app/styles/index.scss";
 
 const Index = () => {
     const [user, setUser] = useState(null);
